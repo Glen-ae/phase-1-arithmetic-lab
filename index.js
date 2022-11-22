69c425a9dec646e69023d0a99b5553e05ae7a8d4
Glen <glen.kimani@student.moringaschool.com>
2022-11-22 11:09:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+
+//random
+const random = Number.isInteger(Math.random()*10)+1
+
+//modulus
+const num3 = 144
+const num4 = 20
+const mod = num3 % num4
+mod
+
+let max = Math.max(9,5,2,20)
+max
 
 // Write your code here
